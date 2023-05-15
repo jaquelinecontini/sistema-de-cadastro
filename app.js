e75340d9955518c56f8ea836a72eab7b2ea0c6cb
jaquelinecontini <jaquelinecontini0@gmail.com>
2023-05-14 22:22:15 -0300

    adicionando consoles

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos.")
 console.log("Aplicação está se conectando ao banco de dados.")
-console.log("O paulo fez esse commit sem você ver na aula!")
 console.log("Enviando para o GitHub")
